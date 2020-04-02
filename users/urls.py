96aaf7fa7a7a1ebe6f6e18fe5265d4d3f24bf6f0
mihsim <mihkel.siim.86@gmail.com>
2020-04-02 13:06:13 +0300

    #11 added functionality: login

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('create/', views.account_create, name='create'),
+    path('login/', views.account_login, name="login"),
     path('logout/', views.account_logout, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 
 # Create your views here.
@@ -18,6 +18,18 @@ def account_create(request):
         return render(request, 'users/create.html')
     else:
         return render(request, 'users/create.html')
+
+
+def account_login(request):
+    if request.method == "GET":
+        return render(request, 'users/login.html')
+    else:
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user:
+            login(request, user)
+            return redirect("project:home")
+        else:
+            return render(request, 'users/login.html', {'error': 'Username and password did not match!'})
 
 
 @login_required
