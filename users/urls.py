38561f3a5117d1994dc75895aa2bbd08887e4dd3
mihsim <mihkel.siim.86@gmail.com>
2020-04-02 17:00:01 +0300

    Added functionality: change password

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('create/', views.account_create, name='create'),
+    path('account/modify/', views.account_modify, name="modify"),
+    path('account/modify/password/', views.change_password, name="change_password"),
     path('login/', views.account_login, name="login"),
     path('logout/', views.account_logout, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, logout, authenticate
-
-
-# Create your views here.
+from django.contrib.auth import login, logout, authenticate, update_session_auth_hash
 
 
 def account_create(request):
@@ -18,6 +14,31 @@ def account_create(request):
         return render(request, 'users/create.html')
     else:
         return render(request, 'users/create.html')
+
+
+@login_required
+def account_modify(request):
+    if request.method == "GET":
+        return render(request, "users/account_modify.html")
+    else:
+        pass
+
+
+@login_required
+def change_password(request):
+    if request.method == "POST":
+        if request.user.check_password(request.POST['old_password']):
+            if request.POST['new_password1'] == request.POST['new_password2']:
+                request.user.set_password(request.POST['new_password1'])
+                request.user.save()
+                update_session_auth_hash(request, request.user)
+                return render(request, 'users/account_modify.html', {'password_changed': 'Password change successful!'})
+            else:
+                return render(request, 'users/account_modify.html', {'password_change_error': 'New passwords did not match!'})
+        else:
+            return render(request, 'users/account_modify.html', {'password_change_error': 'Current password was wrong!'})
+    else:
+        return redirect("users:modify")
 
 
 def account_login(request):
