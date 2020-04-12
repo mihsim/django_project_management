65590b633d0a8e4d8090f2e5789c06cd2026e17a
mihsim <mihkel.siim.86@gmail.com>
2020-04-12 10:25:30 +0300

    Closes #31, closes #33. Created app tasks and moved task model under it.

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Project, Task, Sprint, ProjectParticipants, ProjectParticipantsInvites
+from .models import Project, ProjectParticipants, ProjectParticipantsInvites
+from sprints.models import Sprint
+from tasks.models import Task
 # Register your models here.
 
 admin.site.register((Project, Task, Sprint, ProjectParticipants, ProjectParticipantsInvites))
